2af3bb0edfa226456076e4d3cba1e2d1e547f81b
Bentar Ristianto Sanjaya <49648965+BentarCr00s@users.noreply.github.com>
2024-02-18 12:21:53 +0700

    Flowers

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-onload = () => {
+window.onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
     clearTimeout(c);
   }, 1000);
 };
 
+var audio = document.getElementById("myAudio");
+audio.volume = 0.3;
+var isAudioPlaying = false;
+
+function playAudio() {
+  if (!isAudioPlaying) {
+    audio.play();
+    isAudioPlaying = true;
+  }
+}
+
+document.addEventListener("click", playAudio);
 new TypeIt("#text", {
-  strings: ["Hi Ce I See Chat", "This Is Flower For You", "Hope You Like It"],
+  strings: [
+    "Hi Ce I See Your Chat",
+    "This Is Flower For You",
+    "Hope You Like It",
+  ],
   speed: 50,
   waitUntilVisible: true,
 }).go();
